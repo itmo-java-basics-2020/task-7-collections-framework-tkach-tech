63ef8e53712804020f337acac08f797d204db447
Danil <tkachwork13@gmail.com>
2020-06-07 18:56:46 +0300

    Complete task-7

--- src/main/java/ru/ifmo/collections/KthLargest.java
+++ src/main/java/ru/ifmo/collections/KthLargest.java
@@ -1,18 +1,34 @@
 package ru.ifmo.collections;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  * Design a class to find the kth largest element in a stream. k is from 1 to numbers.length.
  * Note that it is the kth largest element in the sorted order, not the kth distinct element.
- *
+ * <p>
  * Constructor accepts an integer k and an integer array numbers, which contains initial elements from the stream.
  * For each call to the method KthLargest.add(), return the element representing the kth largest element in the stream.
  */
 public class KthLargest {
+
+    private final Queue<Integer> priorityQueue;
+
     public KthLargest(int k, int[] numbers) {
-        // TODO implement
+        this.priorityQueue = new PriorityQueue<>();
+        for (var number : numbers) {
+            priorityQueue.add(number);
+        }
+        while (priorityQueue.size() > k) {
+            priorityQueue.remove();
+        }
     }
 
     public int add(int val) {
-        throw new UnsupportedOperationException(); // TODO implement
+        if (val > priorityQueue.peek()) {
+            priorityQueue.add(val);
+            priorityQueue.remove();
+        }
+        return priorityQueue.peek();
     }
 }
